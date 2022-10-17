44d0f03d3f694b52091c080d35256701408d410e
George Kinsman <george@georgekinsman.com>
2022-10-17 16:10:22 +0100

    Add comment

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
 import type { HandleServerError } from '@sveltejs/kit';
 
 const handleError: HandleServerError = ({ error }) => {
+
+	// This error is always an empty object
 	console.log(`Error: ${JSON.stringify(error)}`);
+	
 	return {
 		message: 'Something went wrong',
 		code: 'oops'
